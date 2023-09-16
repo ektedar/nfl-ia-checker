04316230142645ff3d21f4322af7631f27d5ecd6
ektedar <ektedar_alam@live.com>
2023-09-16 19:16:01 -0400

    wip: added a parameter to determine env

--- src/main.py
+++ src/main.py
@@ -13,7 +13,8 @@ STATUSES = {
 }
 
 
-def get_login_page(cookie_sess_page: TextIO) -> BeautifulSoup:
+def get_login_page(cookie_info: TextIO,
+                   local: bool = True) -> BeautifulSoup:
     """Returns the login page of the provided session.
     This function is expecting a JSON file with the 
     cookie information that includes the login session
@@ -21,17 +22,23 @@ def get_login_page(cookie_sess_page: TextIO) -> BeautifulSoup:
 
     Parameters
     ----------
-    cookie_sess_page : JSON file
+    cookie_info : JSON file or string
         The JSON file that has all the cookie information
-        for the site to be logged in.
+        for the site to be logged in. If this is not local,
+        it will assume the provided input the json object
+        itself. This is because of pipelien related concerns.
+        I didn't want to upload the cookie file somewhere.
 
     Returns
     -------
     BeautifulSoup
         The souped object of the page.
     """
-    with open(cookie_sess_page, 'r') as f:
-        data = json.load(f)
+    if not local:
+        data = json.load(cookie_info)
+    else:
+        with open(cookie_info, 'r') as f:
+            data = json.load(f)
 
     response = requests.get(data['url'],
                             cookies=data['cookies'],
@@ -43,7 +50,7 @@ def get_login_page(cookie_sess_page: TextIO) -> BeautifulSoup:
     return soup
 
 
-def get_players_status() -> dict:
+def get_players_status(local: bool = True) -> dict:
     """Returns a JSON object of all the players that 
     have the Questionable, Inactive, Doubtful, Injured
     Reserved Status.  Only returns the players who are
@@ -54,7 +61,8 @@ def get_players_status() -> dict:
     dict
         A Dictionary object of all the affected players
     """
-    team_page = get_login_page('login_info.json')
+    team_page = get_login_page('login_info.json',
+                               local=local)
     output = {}
     for code, status in STATUSES.items():
         temp = []
